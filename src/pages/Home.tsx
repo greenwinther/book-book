5e8c976c81ff612480d8b9a2e9aea574a62a298d
greenwinther <dennisgw@live.se>
2025-04-29 17:19:16 +0200

    feat: create Home component to display the main page with SearchBar

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import SearchBar from "../components/SearchBar/SearchBar";
+
+const Home = () => {
+	return (
+		<section>
+			<h1>Book Book</h1>
+			<SearchBar />
+		</section>
+	);
+};
+
+export default Home;

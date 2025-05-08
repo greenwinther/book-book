594be0f3a7c3392d2f7add03b7242aecec29d38e
greenwinther <dennisgw@live.se>
2025-05-08 23:37:54 +0200

    feat: implement responsive navbar layout with styling

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,21 +1,30 @@
 import { Outlet, Link } from "react-router-dom";
+import SearchBar from "../components/SearchBar/SearchBar";
+import "./Layout.scss";
 
 const Layout = () => {
 	return (
-		<>
-			<header>
-				<nav>
-					<Link to="/">Home</Link>
-					<Link to="/profile">Profile</Link>
-				</nav>
+		<div className="app-container">
+			<header className="navbar">
+				<div className="navbar-container">
+					<Link to="/" className="navbar-link">
+						Home
+					</Link>
+					<SearchBar />
+					<Link to="/profile" className="navbar-link">
+						Profile
+					</Link>
+				</div>
 			</header>
-			<main>
+
+			<main className="main">
 				<Outlet />
 			</main>
-			<footer>
+
+			<footer className="footer">
 				<p>Book App © 2025</p>
 			</footer>
-		</>
+		</div>
 	);
 };
 

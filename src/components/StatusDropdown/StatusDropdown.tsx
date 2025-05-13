758238d8462b6b193f93c68b327c113bff18cfc8
greenwinther <dennisgw@live.se>
2025-05-13 22:38:29 +0200

    feat: update StatusDropdown component to accept custom className prop for styling

--- src/components/StatusDropdown/StatusDropdown.tsx
+++ src/components/StatusDropdown/StatusDropdown.tsx
@@ -5,12 +5,13 @@ import "./StatusDropdown.scss";
 type Props = {
 	status?: BookStatus;
 	onChange: (newStatus: BookStatus) => void;
+	className?: string;
 };
 
-const StatusDropdown: React.FC<Props> = ({ status = "", onChange }) => {
+const StatusDropdown: React.FC<Props> = ({ status = "", onChange, className }) => {
 	return (
 		<select
-			className="status-dropdown"
+			className={`status-dropdown ${className || ""}`}
 			value={status}
 			onChange={(e) => onChange(e.target.value as BookStatus)}
 		>

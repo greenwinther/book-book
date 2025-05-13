bc5b8058bea7ac55baca927a7ce0ea8cf246b80a
greenwinther <dennisgw@live.se>
2025-05-13 22:36:07 +0200

    feat: add BookMark component for managing favorite items

---
+++ src/components/BookMark/BookMark.tsx
@@ -0,0 +1,22 @@
+import "./BookMark.scss";
+
+type Props = {
+	isFavorite: boolean;
+	onToggle: () => void;
+	className?: string;
+};
+
+const BookMark = ({ isFavorite, onToggle, className }: Props) => (
+	<button
+		className={`bookmark ${isFavorite ? "bookmarked" : ""} ${className || ""}`}
+		onClick={onToggle}
+		title={isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+		aria-label={isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+	>
+		<svg className="bookmark-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M19 21L12 16L5 21V5C5 3.89543 5.89543 3 7 3H17C18.1046 3 19 3.89543 19 5V21Z" />
+		</svg>
+	</button>
+);
+
+export default BookMark;

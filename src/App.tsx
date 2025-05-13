9b8bd1f9dcab57c79647c2786e0aa0e0737196b8
greenwinther <dennisgw@live.se>
2025-05-13 22:39:55 +0200

    refactor: replace StatusProvider with LibraryProvider for improved state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { StatusProvider } from "./contexts/StatusContext";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import Profile from "./pages/profile/Profile";
 import SearchResults from "./pages/searchResults/SearchResults";
 import BookDetails from "./pages/bookDetails/BookDetails";
+import { LibraryProvider } from "./contexts/LibraryContext";
 
 function App() {
 	return (
-		<StatusProvider>
+		<LibraryProvider>
 			<Router>
 				<Routes>
 					<Route path="/" element={<Layout />}>
 						<Route index element={<Home />} />
 						<Route path="profile" element={<Profile />} />
-						<Route path="works/:bookKey" element={<BookDetails />} />
-						<Route path="/search" element={<SearchResults />} />
+						<Route path="book/:bookKey" element={<BookDetails />} />
+						<Route path="search" element={<SearchResults />} />
 					</Route>
 				</Routes>
 			</Router>
-		</StatusProvider>
+		</LibraryProvider>
 	);
 }
 

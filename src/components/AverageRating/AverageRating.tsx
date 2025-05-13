b01d5f05482d538c9d97c6eafa709c29f3b44caa
greenwinther <dennisgw@live.se>
2025-05-13 22:35:58 +0200

    feat: add AverageRating component to display average ratings for books

---
+++ src/components/AverageRating/AverageRating.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { fetchAverageRating } from "../../api/ratings";
+import "./AverageRating.scss";
+
+type RatingProps = {
+	bookKey: string;
+};
+
+const AverageRating = ({ bookKey }: RatingProps) => {
+	const [averageRating, setAverageRating] = useState<number | null>(null);
+
+	useEffect(() => {
+		const getRating = async () => {
+			const average = await fetchAverageRating(bookKey);
+			setAverageRating(average);
+		};
+		getRating();
+	}, [bookKey]);
+
+	if (averageRating === null) return null;
+
+	return (
+		<div
+			className="averagerating"
+			title={`Average Rating: ${averageRating.toFixed(1)}`}
+			aria-label={`Average Rating: ${averageRating.toFixed(1)}`}
+		>
+			<span role="img" aria-hidden="true">
+				🔮
+			</span>{" "}
+			{averageRating.toFixed(1)}
+		</div>
+	);
+};
+
+export default AverageRating;
